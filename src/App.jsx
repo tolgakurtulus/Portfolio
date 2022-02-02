5fb41be66f263afe13f5175dc44c34a70fda5ae4
tolga.kurtulus <tolga.kurtulus@odeontechnology.com>
2022-02-02 17:27:48 +0300

    Add LazySuspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import React from "react";
-import "./App.scss";
+import React, { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import MainPage from "./Pages/MainPage/MainPage.jsx";
-import PortfolioDetail from "./Pages/PortfolioDetail/PortfolioDetail.jsx";
+import LoadingContainer from "./Components/LoadingContainer/LoadingContainer.jsx";
 import DataInfo from "./assets/data/data.js";
+import "./App.scss";
+
+const MainPage = lazy(() => import("./Pages/MainPage/MainPage.jsx"));
+const PortfolioDetail = lazy(() => import("./Pages/PortfolioDetail/PortfolioDetail.jsx"));
 
 function App() {
   return (
     <div className="c-app">
-      <Routes>
-        <Route exact path="/Portfolio" element={<MainPage />} />
-        {DataInfo.map((item) => {
-          return (
-            <Route
-              key={item}
-              exact
-              path={`/Portfolio/${item.titlelink}`}
-              element={<PortfolioDetail item={item} />}
-            />
-          );
-        })}
-      </Routes>
+      <Suspense fallback={<LoadingContainer />}>
+        <Routes>
+          <Route exact path="/Portfolio" element={<MainPage />} />
+          {DataInfo.map((item) => {
+            return (
+              <Route
+                key={item}
+                exact
+                path={`/Portfolio/${item.titlelink}`}
+                element={<PortfolioDetail item={item} />}
+              />
+            );
+          })}
+        </Routes>
+      </Suspense>
     </div>
   );
 }
---
+++ src/Components/LoadingContainer/LoadingContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoadingContainer.scss";
+
+const LoadingContainer = () => {
+  return (
+    <>
+      <span class="smooth spinner" />
+    </>
+  );
+};
+
+export default LoadingContainer;
