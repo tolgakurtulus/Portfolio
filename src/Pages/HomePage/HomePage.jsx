60d38f1583b8a5fb02a620cacb03447c6d9a84bf
Tolga Kurtulus | Odeon Technology <tolga.kurtulus@odeontechnology.com>
2023-11-15 13:35:41 +0300

    Homepage Fix1

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -5,36 +5,32 @@ import "./HomePage.scss";
 const HomePage = () => {
   return (
     <div className="c-homepage">
-      <div className="c-homepage__overlay">
-        <div className="c-homepage-container">
-          <div className="c-homepage-container__row">
-            <div className="c-homepage-container__col">
-              <div className="c-homepage-container__circle">
-                <div className="c-homepage-container__circle-item"></div>
-              </div>
-            </div>
-            <div className="c-homepage-container__welcome">
-              <h1 data-text="Welcome">
-                <mark>Welcome</mark>
-              </h1>
-            </div>
-            <div className="c-homepage-container__typical">
-              <Typical
-                steps={[
-                  `I'm`,
-                  1500,
-                  `I'm Tolga Kurtuluş`,
-                  1500,
-                  `I'm`,
-                  1500,
-                  `I'm Senior Frontend Developer`,
-                  1500,
-                ]}
-                loop={Infinity}
-                wrapper="p"
-              />
-            </div>
+      <div className="c-homepage__row">
+        <div className="c-homepage__col">
+          <div className="c-homepage__circle">
+            <div className="c-homepage__circle-item"></div>
           </div>
+        </div>
+        <div className="c-homepage__welcome">
+          <h1 data-text="Welcome">
+            <mark>Welcome</mark>
+          </h1>
+        </div>
+        <div className="c-homepage__typical">
+          <Typical
+            steps={[
+              `I'm`,
+              1500,
+              `I'm Tolga Kurtuluş`,
+              1500,
+              `I'm`,
+              1500,
+              `I'm Senior Frontend Developer`,
+              1500,
+            ]}
+            loop={Infinity}
+            wrapper="p"
+          />
         </div>
       </div>
     </div>
