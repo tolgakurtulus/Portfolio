721913dd931c0e76f66b5736d71a904b09c61e37
tolgakurtulus <tolgabb4@hotmail.com>
2023-11-27 21:22:43 +0300

    Button Fix

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,17 +1,24 @@
 import React from "react";
-import "./Button.scss";
+import styles from "./styles.module.scss";
 
 const Button = (props) => {
+  const handleClick = (event) => {
+    props.handleClick && props.handleClick(event.target.value);
+  };
 
-    const handleClick = (event) => {
-      props.handleClick && props.handleClick(event.target.value);
-    }
-
-    return (
-        <>
-         <button value={props.value} onClick={handleClick} type="button" className={`c-button ${props.color ? props.color : ""} ${props.radius ? props.radius : ""} ${props.size ? props.size : ""}`} >{props.title}</button>
-        </>
-      );    
+  return (
+    <button
+      value={props.value}
+      onClick={handleClick}
+      type="button"
+      className={` 
+        ${styles[`c-button`]} 
+        ${props.color ? styles[props.color] : ""}
+        `}
+    >
+      {props.title}
+    </button>
+  );
 };
 
 export default Button;
